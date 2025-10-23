b16b17982eb471c955e6b431e6fcdab07b9a97fc
pedrogaspar <pedrogaspar@localhost>
2025-10-23 14:14:39 +0100

    criação da tabela amoti e inserção de valores na mesma

--- teste DBeaver/Scripts/Script.sql
+++ teste DBeaver/Scripts/Script.sql
@@ -1,3 +1,16 @@
 create table aloh (
 id_uti int primary key,
 ano date);
+
+create table amoti(
+num_b serial primary key,
+momento varchar(10));
+
+insert into aloh values (1,'2025-03-21');
+
+select * from aloh;
+
+insert into amoti(momento) values ('agora');
+insert into amoti(momento) values ('logo à noi');
+
+select * from amoti;
